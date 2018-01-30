67ec607ff6a374e59a8221513d32f2f6d8b8576b
ariefmanda <ariefmanda57@gmail.com>
2018-01-30 16:59:21 +0700

    transaksi 20%

--- models/transaksi.js
+++ models/transaksi.js
@@ -2,10 +2,14 @@
 module.exports = (sequelize, DataTypes) => {
   var Transaksi = sequelize.define('Transaksi', {
     UserId: DataTypes.INTEGER,
+<<<<<<< HEAD
     UserId: DataTypes.INTEGER
+=======
+>>>>>>> transaksi 20%
   });
   Transaksi.associate = function(models) {
     Transaksi.belongsTo(models.User)
+    Transaksi.hasMany(models.TransaksiItem)
   }
   return Transaksi;
 };
